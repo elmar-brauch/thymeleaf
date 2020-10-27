4e95ea4fa25f91c9cf52ab0ff6d895da7d6c597d
Elmar Brauch <elmar.brauch@gmail.com>
2020-10-27 20:43:10 +0100

    Model simplified

--- src/main/java/de/bsi/thymeleaf/Item.java
+++ src/main/java/de/bsi/thymeleaf/Item.java
@@ -1,17 +1,11 @@
 package de.bsi.thymeleaf;
 
-import java.util.Date;
-
 public class Item {
 	private String id, name;
-	private Date date = new Date();
 
 	public String getId() { return id; }
 	public void setId(String id) { this.id = id; }
 
 	public String getName() { return name; }
 	public void setName(String name) { this.name = name; }
-	
-	public Date getDate() { return date; }
-	public void setDate(Date date) { this.date = date; }
 }
