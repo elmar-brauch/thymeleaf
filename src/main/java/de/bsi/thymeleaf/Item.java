f279b262c2e2d22a18cfcbbeeef8ababd634ed34
Elmar Brauch <elmar.brauch@gmail.com>
2022-02-02 23:27:05 +0100

    Data stored in HttpSession & HttpSession stored in MongoDB

--- src/main/java/de/bsi/thymeleaf/Item.java
+++ src/main/java/de/bsi/thymeleaf/Item.java
@@ -1,6 +1,11 @@
 package de.bsi.thymeleaf;
 
-public class Item {
+import java.io.Serializable;
+
+public class Item implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
 	private String id, name;
 
 	public String getId() { return id; }
--- src/main/java/de/bsi/thymeleaf/ItemController.java
+++ src/main/java/de/bsi/thymeleaf/ItemController.java
@@ -3,6 +3,9 @@ package de.bsi.thymeleaf;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +15,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ItemController {
-
-	private List<Item> items = new ArrayList<>();
 	
 	private static final String MODEL_KEY_ITEMS = "items";
 	private static final String VIEW_NAME_LIST = "item-list";
@@ -22,7 +23,7 @@ public class ItemController {
 	@GetMapping(path = {"/", "/item"})
 	public ModelAndView showItems(ModelAndView mav) {
 		mav.setViewName(VIEW_NAME_LIST);
-		mav.addObject(MODEL_KEY_ITEMS, items);
+		mav.addObject(MODEL_KEY_ITEMS, readItemsFromSession());
 		return mav;
 	}
 	
@@ -34,11 +35,28 @@ public class ItemController {
 	@PostMapping("/item") 
 	public String createNewItem(Model model,
 			@RequestParam String itemname, @RequestParam String itemid) {
-		var item = new Item();
-		item.setName(itemname);
-		item.setId(itemid);
-		items.add(item);
-		model.addAttribute(MODEL_KEY_ITEMS, items);
+		createItemInSession(itemname, itemid);
+		model.addAttribute(MODEL_KEY_ITEMS, readItemsFromSession());
 		return VIEW_NAME_LIST;
+	}
+
+	@Autowired private HttpSession session;
+	
+	private List readItemsFromSession() {
+		if (session.getAttribute(MODEL_KEY_ITEMS) instanceof List items)
+			return items;
+		return List.of();
+	}
+	
+	private void createItemInSession(String name, String id) {
+		var items = new ArrayList<Item>();
+		var obj = session.getAttribute(MODEL_KEY_ITEMS);
+		if (obj instanceof ArrayList) 
+			items = (ArrayList<Item>) obj;
+		var item = new Item();
+		item.setId(id);
+		item.setName(name);
+		items.add(item);
+		session.setAttribute(MODEL_KEY_ITEMS, items);
 	}
 }
