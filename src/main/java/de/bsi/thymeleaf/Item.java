555a7d4737837f3d51323dd1a28f4d47b86ef5aa
Elmar Brauch <elmar.brauch@telekom.de>
2022-08-28 00:40:21 +0200

    ...

--- src/main/java/de/bsi/thymeleaf/Item.java
+++ src/main/java/de/bsi/thymeleaf/Item.java
@@ -1,10 +1,6 @@
 package de.bsi.thymeleaf;
 
-import java.io.Serializable;
-
-public class Item implements Serializable {
-	
-	private static final long serialVersionUID = 1L;
+public class Item {
 	
 	private String id, name;
 
